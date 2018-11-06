7d2c325223ae8e3610909d9b5ea87d53f40e0e8b
Dobroslav <dobs7zz@outlook.com>
2018-11-06 21:12:13 +0200

    Added some methods

--- BookLibrary/Logic/BookService.cs
+++ BookLibrary/Logic/BookService.cs
@@ -28,6 +28,7 @@ namespace BookLibrary.Logic
             return result;
         }
 
+        //Търси книги по зададени филтри за име и/или автор на книгата
         //Викаме горният метод и филтрираме по име
         //.....
 
@@ -39,11 +40,23 @@ namespace BookLibrary.Logic
             return result;
         }
 
-        public void List<Book> AddBook(Book book)
+        List<Book> books = new List<Book>();
+        public void AddBook(string Title, string Author, string Genre, string Description, int Quantity, DateTime CreatedDate) // adding book
         {
-            ctx.AddBook(Book);
-            ctx.SaveChanges();
+            books.Add(new Book(Title, Author, Genre, Description, Quantity, CreatedDate));
         }
+
+        // Редактиране на информацията за книга Void EditBook(Book book)
+
+        public void DeleteBook(string Title) // deleting book
+        {
+            books.RemoveAll(Book => Book.Title == Title);
+        }
+
+        // Списък с кои книги не са върнати в срок List<Book> GetNotReturnedBooks()
+
+        // Списък с кои книги е взел даден читател - търсене по ID на читател List<Book> GetBorrowedBooksByReader(int readerID)
+
 
     }
 }
---
+++ BookLibrary/Logic/ReaderService.cs
@@ -0,0 +1,29 @@
+﻿using BookLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Logic
+{
+    class ReaderService
+    {
+        List<Reader> books = new List<Reader>();
+        public void AddBook(string FirstName, string LastName, string PhoneNumber, DateTime CreatedDate, DateTime ReturnedDate) // adding reader
+        {
+            books.Add(new Reader(FirstName, LastName, PhoneNumber, CreatedDate, ReturnedDate));
+        }
+
+        // Редактиране информацията за читател на библиотеката Void EditReader(Reader reader)
+
+        public void DeleteReader(int id) // deleting reader
+        {
+            books.RemoveAll(Reader => Reader.id == id);
+        }
+
+        // Заемане на книга от читател Void BorrowBook(int bookID, int readerID)
+
+        // Връщане на книга от читател Int ReturnBook(int bookID, int readerID)
+    }
+}
